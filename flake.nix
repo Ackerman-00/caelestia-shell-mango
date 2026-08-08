563714ea38980f4043c3c89da73f35f2ad6c82ab
Ackerman-00 <quietcraft@gmail.com>
2026-08-08 21:06:43 +0600

    flake: point caelestia-cli input at Ackerman-00/caelestia-cli-mango

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     };
 
     caelestia-cli = {
-      url = "github:caelestia-dots/cli";
+      url = "github:Ackerman-00/caelestia-cli-mango";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.caelestia-shell.follows = "";
     };
